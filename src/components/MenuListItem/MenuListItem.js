08f2da0ccbfc0b3a890032eadf7d04d5e693cf5d
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-08-14 00:32:08 -0500

    Fix css on logo

--- src/components/MenuListItem/MenuListItem.js
+++ src/components/MenuListItem/MenuListItem.js
@@ -5,8 +5,8 @@ export default function MenuListItem({ menuItem, handleAddToOrder }) {
 		<div className={styles.MenuListItem}>
 			<div className={styles.name}>{menuItem.name}</div>
 			<div className={styles.buy}>
-				<span>${menuItem.price.toFixed(2)}</span>
 				<div className={styles.description}>{menuItem.description}</div>
+				<span>${menuItem.price.toFixed(2)}</span>
 				<button
 					className="btn-sm"
 					onClick={() => handleAddToOrder(menuItem._id)}
